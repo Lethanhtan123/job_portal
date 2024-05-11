1a62be4d4111b4fd92d839237eac90323841397c
queestraan <queestraan0211@gmail.com>
2024-05-12 00:40:02 +0700

    complete update profile candidate

--- app/Http/Controllers/Fontend/CandidateProfileController.php
+++ app/Http/Controllers/Fontend/CandidateProfileController.php
@@ -3,12 +3,104 @@
 namespace App\Http\Controllers\Fontend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Fontend\CandidateBasicProfileUpdateRequest;
+use App\Http\Requests\Fontend\CandidateProfileInfoUpdateRequest;
+use App\Models\Candidate;
+use App\Models\CandidateLanguage;
+use App\Models\CandidateSkill;
+use App\Models\Experience;
+use App\Models\Language;
+use App\Models\Profession;
+use App\Models\Skill;
+use App\Services\Notify;
+use App\Traits\FileUploadTrait;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class CandidateProfileController extends Controller
 {
-    function index() : View {
-        return view('fontend.candidate-dashboard.profile.index');
+
+    use FileUploadTrait;
+
+    function index(): View
+    {
+        $candidate = Candidate::with(['skills'])->where('user_id', auth()->user()->id)->first();
+        $experiences = Experience::all();
+        $professions = Profession::all();
+        $skills = Skill::all();
+        $languages = Language::all();
+        return view('fontend.candidate-dashboard.profile.index', compact('candidate', 'experiences', 'professions', 'skills', 'languages'));
+    }
+
+    // uppdate basic info of cadidate
+    function basicInfoUpdate(CandidateBasicProfileUpdateRequest $request): RedirectResponse
+    {
+
+        $imagePath = $this->uploadFile($request, 'profile_picture');
+        $cvPath = $this->uploadFile($request, 'cv');
+
+        $data = [];
+        if (!empty($imagePath)) $data['image'] = $imagePath;
+        if (!empty($cvPath)) $data['cv'] = $cvPath;
+        $data['full_name'] = $request->full_name;
+        $data['title'] = $request->title;
+        $data['experience_id'] = $request->experience_level;
+        $data['website'] = $request->website;
+        $data['birth_date'] = $request->birth_date;
+
+        Candidate::updateOrCreate(
+            ['user_id' => auth()->user()->id],
+            $data
+        );
+
+        Notify::updatedNotifycation();
+
+        return redirect()->back();
+    }
+
+    // update profile candidate
+    function profileInfoUpdate(CandidateProfileInfoUpdateRequest $request): RedirectResponse
+    {
+
+        $data = [];
+
+        $data['gender'] = $request->gender;
+        $data['marital_status'] = $request->marital_status;
+        $data['profession_id'] = $request->profession;
+        $data['status'] = $request->availability;
+        $data['bio'] = $request->biography;
+
+        Candidate::updateOrCreate(
+            ['user_id' => auth()->user()->id],
+            $data
+        );
+
+
+        $candidate = Candidate::where('user_id', auth()->user()->id)->first();
+
+        CandidateLanguage::where('candidate_id', $candidate->id)?->delete();
+
+        foreach ($request->language_you_know as $lang) {
+            $candidateLang = new CandidateLanguage();
+            $candidateLang->candidate_id = $candidate->id;
+            $candidateLang->language_id = $lang;
+            $candidateLang->save();
+        }
+
+        CandidateSkill::where('candidate_id', $candidate->id)?->delete();
+
+        foreach ($request->skill as $skillC) {
+            $candidateSkill = new CandidateSkill();
+            $candidateSkill->candidate_id =  $candidate->id;
+            $candidateSkill->skill_id = $skillC;
+            $candidateSkill->save();
+        }
+
+        // $this->updateProfileStatus();
+
+        Notify::updatedNotifycation();
+
+        return redirect()->back();
     }
 }
--- resources/views/fontend/layouts/master.blade.php
+++ resources/views/fontend/layouts/master.blade.php
@@ -87,6 +87,8 @@
     <script src="{{ asset('fontend/assets/js/plugins/counterup.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="{{ asset('fontend/assets/js/main.js?v=4.1') }}"></script>
+    <script src="https://cdn.ckeditor.com/ckeditor5/41.3.1/classic/ckeditor.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"
         integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA=="
@@ -101,6 +103,13 @@
              format: 'yyyy-mm-dd',
 
         });
+
+         ClassicEditor
+        .create( document.querySelector( '#editor' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+
         /*
                 function confirmation(ev) {
                     ev.preventDefault();
