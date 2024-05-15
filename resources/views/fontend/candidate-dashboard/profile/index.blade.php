acf98d73bafb9ffd994218c04e1468daff3ca845
queestraan <queestraan0211@gmail.com>
2024-05-16 03:14:29 +0700

    form add experience

--- resources/views/fontend/candidate-dashboard/profile/index.blade.php
+++ resources/views/fontend/candidate-dashboard/profile/index.blade.php
@@ -1,58 +1,121 @@
 @extends('fontend.layouts.master')
 @section('contents')
-<section class="section-box mt-75">
-    <div class="breacrumb-cover">
+    <section class="section-box mt-75">
+        <div class="breacrumb-cover">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-12">
+                        <h2 class="mb-20">My Profile</h2>
+                        <ul class="breadcrumbs">
+                            <li><a class="home-icon" href="index.html">Home</a></li>
+                            <li>My Profile</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-box mt-120">
         <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-12">
-                    <h2 class="mb-20">My Profile</h2>
-                    <ul class="breadcrumbs">
-                        <li><a class="home-icon" href="index.html">Home</a></li>
-                        <li>My Profile</li>
+            <div class="row">
+                @include('fontend.candidate-dashboard.sidebar')
+                <div class="col-lg-9 col-md-8 col-sm-12 col-12 mb-50">
+
+                    <ul class="mb-3 nav nav-pills nav-cus" id="pills-tab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill"
+                                data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home"
+                                aria-selected="true">Basic</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill"
+                                data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile"
+                                aria-selected="false">Profile</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="pills-experience-tab" data-bs-toggle="pill"
+                                data-bs-target="#pills-experience" type="button" role="tab"
+                                aria-controls="pills-experience" aria-selected="false">Exeperience & Education</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill"
+                                data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact"
+                                aria-selected="false">Account Settings</button>
+                        </li>
                     </ul>
+                    <div class="tab-content" id="pills-tabContent">
+                        @include('fontend.candidate-dashboard.profile.sections.basic-sections')
+                        @include('fontend.candidate-dashboard.profile.sections.profile-sections')
+                        @include('fontend.candidate-dashboard.profile.sections.experience-sections')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="modal fade" id="experienceModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Experience</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="" method="">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label for="">Company</label>
+                                    <input type="text" class="form-control" name="" id="">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="">Department</label>
+                                    <input type="text" class="form-control" name="" id="">
+                                </div>
+                            </div>
+                             <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="">Designation</label>
+                                    <input type="text" class="form-control" name="" id="">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="">Start Date</label>
+                                    <input type="text" class="form-control datepicker" name="" id="">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="">End Date</label>
+                                    <input type="text" class="form-control datepicker" name="" id="">
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group form-check form-check-inline">
+                                    <input type="checkbox" class=" form-check-input me-3 "  name="" id="">
+                                    <label class=" form-check-label" for="">I'm currently working</label>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label for="">Responsibilities</label>
+                                    <textarea class="form-control" name="" id=""></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Add Experience</button>
                 </div>
             </div>
         </div>
     </div>
-</section>
-
-<section class="section-box mt-120">
-    <div class="container">
-        <div class="row">
-            @include('fontend.candidate-dashboard.sidebar')
-            <div class="col-lg-9 col-md-8 col-sm-12 col-12 mb-50">
-
-                <ul class="mb-3 nav nav-pills nav-cus" id="pills-tab" role="tablist">
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill"
-                            data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home"
-                            aria-selected="true">Basic</button>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill"
-                            data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile"
-                            aria-selected="false">Profile</button>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="pills-experience-tab" data-bs-toggle="pill"
-                            data-bs-target="#pills-experience" type="button" role="tab" aria-controls="pills-experience"
-                            aria-selected="false">Exeperience & Education</button>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill"
-                            data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact"
-                            aria-selected="false">Account Settings</button>
-                    </li>
-                </ul>
-                <div class="tab-content" id="pills-tabContent">
-                    @include('fontend.candidate-dashboard.profile.sections.basic-sections')
-                    @include('fontend.candidate-dashboard.profile.sections.profile-sections')
-                    @include('fontend.candidate-dashboard.profile.sections.experience-sections')
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
 @endsection
 
 {{-- @push('scripts')
