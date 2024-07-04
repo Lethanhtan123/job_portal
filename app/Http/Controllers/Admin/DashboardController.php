97d7c4c38c55906d76544633af9b058218553d1d
queestraan <queestraan0211@gmail.com>
2024-07-05 03:57:02 +0700

    update

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,13 +3,25 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use App\Models\Candidate;
+use App\Models\Company;
+use App\Models\Job;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
-    function index() : View{
+    function index(): View
+    {
+        $totalCandidate = Candidate::count();
+        $totalCompany = Company::count();
+        $totalBlogs = Blog::count();
+        $totalJobs = Job::count();
+        $totalActiveJobs = Job::where('deadline', '>=', date('Y-m-d'))->count();
+        $totalExpiredJobs = Job::where('deadline', '<', date('Y-m-d'))->count();
+        $totalPendingJobs = Job::where('status','pending')->count();
 
-        return view('admin.dashboard.index');
+        return view('admin.dashboard.index', compact('totalCandidate', 'totalCompany', 'totalJobs', 'totalActiveJobs', 'totalExpiredJobs','totalPendingJobs','totalBlogs'));
     }
 }
