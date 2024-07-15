eb6dd9a29db329cb19d1c4704b81df32df7560e9
Lethanhtan123 <lethanhtan129@gmail.com>
2024-07-16 03:14:53 +0700

    fixbug

--- resources/views/fontend/layouts/footer.blade.php
+++ resources/views/fontend/layouts/footer.blade.php
@@ -5,36 +5,11 @@
             $footerDetails = \App\Models\Footer::first();
             $footerIcons = \App\Models\SocialIcon::all();
 
-<<<<<<<<< Temporary merge branch 1
-    @endphp
-        <div class="row_footer">
-            <a class="footer_logo" href="index.html">
-                <img alt="dreamjob" src="{{ $footerDetails?->logo }}">
-            </a>
-            <div class="mt-20 mb-20 footer_desc font-xs color-text-paragraph-2">{{ $footerDetails?->details }}</div>
-            <div class="footer-social">
-                @foreach ($footerIcons as $icon)
-                <a class="icon-socials icon-facebook" href="{{ $icon->url }}"><i class="{{ $icon->icon }}"></i></a>
-                @endforeach
 
-            </div>
-        </div>
-      </div>
-      <div class="footer-bottom mt-50">
-        <div class="row">
-            <div class="col-md-6"><span class="font-xs color-text-paragraph">&copy; Copyright 2024 {{ $footerDetails?->copyright }}. All rights reserved.</span></div>
-        </div>
-    </div>
-    </div>
-
-    <a class="btn-map btn-frame text-decoration-none" target="_blank" href="https://www.messenger.com/t/306383955902729"
-        style="bottom:190px;">
-=========
             @endphp
-
             <div class="row_footer">
                 <a class="footer_logo" href="index.html">
-                    <img alt="joblist" src="{{ $footerDetails?->logo }}">
+                    <img alt="dreamjob" src="{{ $footerDetails?->logo }}">
                 </a>
                 <div class="mt-20 mb-20 footer_desc font-xs color-text-paragraph-2">{{ $footerDetails?->details }}</div>
                 <div class="footer-social">
@@ -42,9 +17,35 @@
                     <a class="icon-socials icon-facebook" href="{{ $icon->url }}"><i class="{{ $icon->icon }}"></i></a>
                     @endforeach
 
-
                 </div>
             </div>
+        </div>
+        <div class="footer-bottom mt-50">
+            <div class="row">
+                <div class="col-md-6"><span class="font-xs color-text-paragraph">&copy; Copyright 2024 {{
+                        $footerDetails?->copyright }}. All rights reserved.</span></div>
+            </div>
+        </div>
+    </div>
+
+    <a class="btn-map btn-frame text-decoration-none" target="_blank" href="https://www.messenger.com/t/306383955902729"
+        style="bottom:190px;">
+
+        @endphp
+
+        <div class="row_footer">
+            <a class="footer_logo" href="index.html">
+                <img alt="joblist" src="{{ $footerDetails?->logo }}">
+            </a>
+            <div class="mt-20 mb-20 footer_desc font-xs color-text-paragraph-2">{{ $footerDetails?->details }}</div>
+            <div class="footer-social">
+                @foreach ($footerIcons as $icon)
+                <a class="icon-socials icon-facebook" href="{{ $icon->url }}"><i class="{{ $icon->icon }}"></i></a>
+                @endforeach
+
+
+            </div>
+        </div>
         </div>
 
         <div class="footer-bottom mt-50">
@@ -54,20 +55,19 @@
 
             </div>
         </div>
-    </div>
+        </div>
 
-   <a class="btn-map btn-frame text-decoration-none" target="_blank" href="https://www.messenger.com/t/306383955902729"
-        style="bottom:400px;">
->>>>>>>>> Temporary merge branch 2
-        <div class="animated infinite zoomIn kenit-alo-circle"></div>
-        <div class="animated infinite pulse kenit-alo-circle-fill"></div>
-        <i><img src="{{ asset('fontend/assets/imgs/template/mess.png') }}" alt="">
-        </i>
-    </a>
+        <a class="btn-map btn-frame text-decoration-none" target="_blank"
+            href="https://www.messenger.com/t/306383955902729" style="bottom:400px;">
 
-<<<<<<<<< Temporary merge branch 1
-  </footer>
+            <div class="animated infinite zoomIn kenit-alo-circle"></div>
+            <div class="animated infinite pulse kenit-alo-circle-fill"></div>
+            <i><img src="{{ asset('fontend/assets/imgs/template/mess.png') }}" alt="">
+            </i>
+        </a>
 
-=========
+
 </footer>
->>>>>>>>> Temporary merge branch 2
+
+
+</footer>
